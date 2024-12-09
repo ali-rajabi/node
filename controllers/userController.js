3cf7700bd70ed1e20bb8d2bf8f17e07db33b38d6
a.rajabi <a.rajabi>
2024-12-09 14:36:43 +0330

    implement file structure

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+// const users = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/users.json`)
+// );
+
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet definded!',
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet definded!',
+  });
+};
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet definded!',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet definded!',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet definded!',
+  });
+};
