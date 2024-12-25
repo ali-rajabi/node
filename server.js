93471a4920ce2d86b41b932ca01828fffb4bf7ab
a.rajabi <a.rajabi>
2024-12-25 16:26:53 +0330

    crud tours added

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose.connect(DB).then(() => console.log('DB connection success'));
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection success'))
+  .catch((e) => console.log(e));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
