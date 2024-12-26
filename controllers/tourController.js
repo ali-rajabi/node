89a5de9323c08b0440c7250e018c12bf6d2984f7
a.rajabi <a.rajabi>
2024-12-26 15:34:26 +0330

    advance filter added to tour routes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,7 +21,35 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // console.log(req.query);
+
+    // BUILD QUERY
+    // 1) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    // gte , gt , lte , lt
+
+    // { difficulty: 'easy', duration: { $gte: '5' } }
+    // { difficulty: 'easy', duration: { gte: '5' } }
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours?.length,
