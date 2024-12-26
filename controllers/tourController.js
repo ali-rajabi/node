22e1f72063d929a3f12a52ef119087ff9b9af51b
a.rajabi <a.rajabi>
2024-12-26 15:56:41 +0330

    sorting added to tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,24 +21,39 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    // console.log(req.query);
+    console.log(req.query);
 
     // BUILD QUERY
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2) Advanced filtering
+    // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
     // gte , gt , lte , lt
-
     // { difficulty: 'easy', duration: { $gte: '5' } }
     // { difficulty: 'easy', duration: { gte: '5' } }
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+    // if (req.query.page) {
+    //   query = query.page(req.query.page);
+    // }
+    // if (req.query.limit) {
+    //   query = query.limit(req.query.limit);
+    // }
+    // if (req.query.fields) {
+    //   query = query.fields(req.query.fields);
+    // }
 
     // EXECUTE QUERY
     const tours = await query;
